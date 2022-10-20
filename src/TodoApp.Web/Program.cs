9eeb38e460854284ad509ad8296bf36381a6b635
Yanzhi Li <liyanzhi@gmail.com>
2022-10-20 09:35:02 +0800

    Add some sample code

--- src/TodoApp.Web/Program.cs
+++ src/TodoApp.Web/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Runtime.Intrinsics.Arm;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using Serilog.Events;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace TodoApp.Web;
 
@@ -24,6 +28,23 @@ public class Program
             .WriteTo.Async(c => c.File("Logs/logs.txt"))
             .WriteTo.Async(c => c.Console())
             .CreateLogger();
+        
+        string original = "Here is some data to encrypt!";
+
+        //c# Encrypt with AES
+        using (System.Security.Cryptography.Aes myAes = System.Security.Cryptography.Aes.Create())
+        {
+
+            // Encrypt the string to an array of bytes.
+            byte[] encrypted = EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+
+            // Decrypt the bytes to a string.
+
+            //Display the original data and the decrypted data.
+            Console.WriteLine("Original:   {0}", original);
+            
+        }
+
 
         try
         {
@@ -48,4 +69,43 @@ public class Program
             Log.CloseAndFlush();
         }
     }
-}
+
+    static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+    {
+        // Check arguments.
+        if (plainText == null || plainText.Length <= 0)
+            throw new ArgumentNullException("plainText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+        byte[] encrypted;
+
+        // Create an Aes object
+        // with the specified key and IV.
+        using (System.Security.Cryptography.Aes aesAlg = System.Security.Cryptography.Aes.Create())
+        {
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+        }
+
+        // Return the encrypted bytes from the memory stream.
+        return encrypted;
+    }
